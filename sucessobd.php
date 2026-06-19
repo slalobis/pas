c6539933012a7dd764756479abd09e3fb340a2ae
slalobis <joaotatsch5208@gmail.com>
2026-06-19 09:08:25 -0300

    baixar php em casa

---
+++ sucessobd.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+    <html lang="pt-BR">
+<head>
+<meta charset="UTF-8">
+    <title>Sucesso Banco de Dados</title>
+</head>
+<body>
+    <h2>Sucesso ao Gravar no Banco de Dados MySQL!</h2>
+    <a href="index.php" title="Ir para pagina inicial">
+    <img src="src/sucessobd.png" alt="index.php" width="600" height="500">
+</a>
+</body>
+</html>
